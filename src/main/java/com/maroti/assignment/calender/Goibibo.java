8f51a464f10147f58ba47db1e511d7ecd7035659
marotipawar <pawarmw@gmail.com>
2023-06-23 06:37:51 +0530

    goibibo assignment completed

--- src/main/java/com/maroti/assignment/calender/Goibibo.java
+++ src/main/java/com/maroti/assignment/calender/Goibibo.java
@@ -1,13 +1,17 @@
 package com.maroti.assignment.calender;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Goibibo {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         //Step 1: Up casting
         WebDriver driver = new ChromeDriver();
         //Step 2: Open a https://www.goibibo.com/
@@ -31,19 +35,73 @@ public class Goibibo {
         WebElement selectToCity=driver.findElement(By.xpath("//ul[@class='sc-12foipm-39 dMRdWf']/li[1]"));
         selectToCity.click();
         //step 5: select departure date as 10-Aug-2023 value
+        boolean result = false;
 
-        WebElement nextMonth=driver.findElement(By.xpath("//div[@class='DayPicker']/div/child::div[1]/span[2]"));
+            do{
+                WebElement months=driver.findElement(By.xpath("//div[@class='DayPicker-Month'][1]/div[1]/div"));
+                WebElement nextMonth=driver.findElement(By.xpath("//div[@class='DayPicker-NavBar']/span[2]"));
+                List<WebElement> days=driver.findElements(By.xpath("//div[@class='DayPicker-Month'][1]/div[3]//child::div/p[1]"));
+
+                if(months.getText().equals("May 2024")){
+                    result=true;
+                    days.forEach(day->{
+                        if(day.getText().equals("31")){
+                            day.click();
+                        }
+                    });
+                }else {
+                    nextMonth.click();
+                }
+            }while(!result);
+
+            driver.findElement(By.xpath("//div[@class='sc-12foipm-54 bbkygy']/span[2]")).click();
+
+
 
         //step 6: select 5 Adults 4 children
+
+        Actions act = new Actions(driver);
+        WebElement addAdults=driver.findElement(By.xpath("//div[@class='sc-12foipm-33 CAJUg']/div[2]/div[2]/div/div[1]/div[1]/div/span[3]"));
+        act.click(addAdults).click(addAdults).click(addAdults).click(addAdults).build().perform();
+
+        WebElement addChildren=driver.findElement(By.xpath("//div[@class='sc-12foipm-33 CAJUg']/div[2]/div[2]/div/div[1]/div[2]/div/span[3]"));
+        act.click(addChildren).click(addChildren).click(addChildren).click(addChildren).build().perform();
+
         //step 7: select Travel class as "Premium Economy"
+        driver.findElement(By.xpath("//div[@class='sc-12foipm-33 CAJUg']/div[2]/div[2]/div/div[2]/ul/li[2]")).click();
+
         //step 8: click on Done
+        driver.findElement(By.xpath("//div[@class='sc-12foipm-33 CAJUg']/div[2]/div[3]/a[2]")).click();
 
         //step 9: Take screenshot
 
+        TakesScreenshot shot = (TakesScreenshot)driver;
+        File src =shot.getScreenshotAs(OutputType.FILE);
+        new File("img").mkdir();
+        FileUtils.copyFile(src, new File("img","screenshot-1.png"));
         //step 10: click on search flights
+        driver.findElement(By.xpath("//div[@class='sc-1gt8xf5-3 cBVNVf']/div/div[3]/span")).click();
 
         //step 11: take screenshot
-
+        File searchSrc=shot.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(src, new File("img", "search.png"));
         //step 12: capture all flight price high to low and print in console.
+        Thread.sleep(15000);
+        WebElement prices=driver.findElement(By.xpath("//div[@class='seo-srp-layoutstyles__RightWrap-sc-11ypfer-3 isaGih']/div[2]/div[1]/div[2]/div/div[4]"));
+        prices.click();
+        List<WebElement> priceHighest=driver.findElements(By.xpath("//div[@class='srp-card-uistyles__Price-sc-3flq99-17 HRAwb alignItemsCenter dF fb lh1 padT5']"));
+        System.out.println("Highest flight Prices");
+        System.out.println("-----------------------------------------------------");
+        priceHighest.forEach(item->{
+            System.out.println("Highest Prices :"+item.getText());
+        });
+        prices.click();
+        List<WebElement> priceLowest=driver.findElements(By.xpath("//div[@class='srp-card-uistyles__Price-sc-3flq99-17 HRAwb alignItemsCenter dF fb lh1 padT5']"));
+        System.out.println("Lowest flight Prices");
+        System.out.println("-----------------------------------------------------");
+        priceLowest.forEach(item->{
+            System.out.println("Lowest Prices :"+item.getText());
+        });
+
     }
 }
---
+++ src/main/java/com/maroti/assignment/window/WindowHandle.java
@@ -0,0 +1,62 @@
+package com.maroti.assignment.window;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class WindowHandle {
+    public static void main(String[] args) throws InterruptedException {
+
+        //Step 1: Up casting
+        WebDriver driver = new ChromeDriver();
+
+        //step 2: open a URL https://www.hyrtutorials.com/p/window-handles-practice.html
+
+        driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
+        driver.manage().window().maximize();
+        //step 3: capture title
+
+        System.out.println(driver.getTitle());
+        //step 4: capture parent window Id number
+
+        String parentWindowId =driver.getWindowHandle();
+        System.out.println(parentWindowId);
+
+        //step 5: click on open a new window
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//article/div/div//child::div[2]/button")).click();
+
+        //step 6: capture opened window Id Number
+        AtomicReference<String> newWindowId=new AtomicReference<String>();
+        driver.getWindowHandles()
+                .forEach(win->{
+                    if(!win.equals(parentWindowId))
+                    {
+                        System.out.println("New Window Id :"+win);
+                        newWindowId.set(win);
+                    }
+        });
+
+        // step 7: switch focus to Opened window
+        driver.switchTo().window(newWindowId.get()).manage().window().maximize();
+
+        //Step 8: Enter firsname, last name, select gender and select checkbox
+        driver.findElement(By.xpath("//input[@name='fName']")).sendKeys("Maroti");
+        driver.findElement(By.xpath("//input[@name='lName']")).sendKeys("Pawar");
+        driver.findElement(By.xpath("//input[@id='malerb']")).click();
+        driver.findElements(By.xpath("//input[@name='language']"))
+                .forEach(checkbox->{
+                    checkbox.click();
+                });
+        Thread.sleep(8000);
+
+        //step 9: switch focus to parent Window
+        driver.switchTo().window(parentWindowId);
+
+        //step 10: capture URL
+        System.out.println("Capture URL :"+driver.getCurrentUrl());
+    }
+
+}
